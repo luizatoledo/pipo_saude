e94d37079aa9de4b13e0e469b554660e77a33a1d
Luiza Toledo <luiza.toledo96@gmail.com>
2021-01-24 18:20:32 -0300

    Added more associations to Partner model after crash test in console

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,5 +1,5 @@
 class Partner < ApplicationRecord
   has_many :contracts, dependent: :destroy
-  # has_many :clients, through: :contracts, dependent: :destroy
-  # has_many :employees, through: :clients, dependent: :destroy
+  has_many :clients, through: :contracts, dependent: :destroy
+  has_many :employees, through: :clients, dependent: :destroy
 end
