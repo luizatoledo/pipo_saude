fe46bf756132d7c4fa1ab25c0335b586172d042c
Luiza Toledo <luiza.toledo96@gmail.com>
2021-01-25 10:47:10 -0300

    Basic Routes, Controller Actions and View files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+
+  # employees controller
+  resources :employees, only: [:new, :create, :show]
 end
