79922e619edd096e3c429e440c9e47e0c1ab4df8
Luiza Toledo <luiza.toledo96@gmail.com>
2021-01-28 15:43:17 -0300

    React actions

---
+++ app/javascript/registration/actions/index.js
@@ -0,0 +1,39 @@
+// export const of action types
+export const FETCH_EMPLOYEES = 'FETCH_EMPLOYEES';
+export const FETCH_EMPLOYEE = 'FETCH_EMPLOYEE';
+export const CREATE_EMPLOYEE = 'CREATE_EMPLOYEE';
+
+export function fetchEmployees() {
+  const promise = fetch("/api/v1/employees").then(response => response.json());
+
+  return {
+    type: FETCH_EMPLOYEES,
+    payload: promise
+  };
+}
+
+export function fetchEmployee(id) {
+  const promise = fetch(`/api/v1/employees/${id}`).then(response => response.json());
+
+  return {
+    type: FETCH_EMPLOYEE,
+    payload: promise
+  };
+}
+
+export function createEmployee(info, callback) {
+  const request = fetch("/api/v1/employees", {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(info)
+  }).then(response => response.json())
+    .then(callback);
+
+  return {
+    type: CREATE_EMPLOYEE,
+    payload: request
+  };
+}
