66a00bc5ff3aff5103dec3a8fe8f687075d8a9f6
Luiza Toledo <luiza.toledo96@gmail.com>
2021-02-01 02:02:56 -0300

    added uniqueness validation to cpf

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,16 +1,5 @@
 class EmployeesController < ApplicationController
   def show
     @employee = Employee.find(params[:id])
-    
-    @employee_offers = []
-    @employee.client.contracts.each do |c|
-      c.partner.offers.to_a.each { |offer| @employee_offers << offer.name}
-    end
-    
-    # @info = [] 
-    # @employee_offers.each do |o|
-    #   o[0].registration_data.each { |d| @info << JSON.parse(d).symbolize_keys!}
-    # end
-    # @info = @info.uniq
   end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,5 @@
 class Employee < ApplicationRecord
   belongs_to :client
   validates :cpf, presence: true
+  validates :cpf, uniqueness: true
 end
